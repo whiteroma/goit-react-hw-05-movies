ee330f80dd1ac8e61bbf85783d350b7467f77bfa
whiteroma <roma.white95@gmail.com>
2022-10-25 12:10:32 +0200

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Home from './Home'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+    </Routes>
+    </>
   );
 };
